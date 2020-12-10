0e68b04b7e4b9f1d36e3c000cb6d7008c8cce410
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2020-12-10 18:53:39 +0900

    update

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// File: main.cpp
+// Project: test
+// Created Date: 10/12/2020
+// Author: Shun Suzuki
+// -----
+// Last Modified: 10/12/2020
+// Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+// -----
+// Copyright (c) 2020 Hapis Lab. All rights reserved.
+//
+
+#include "lib.h"
+
+int main() { SayHello(); }
