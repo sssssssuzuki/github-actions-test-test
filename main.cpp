18a119e19b6295aa2a3480a89622389e4c64a354
Shun Suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2021-02-19 20:59:26 +0900

    update

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 // Created Date: 10/12/2020
 // Author: Shun Suzuki
 // -----
-// Last Modified: 10/12/2020
+// Last Modified: 19/02/2021
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2020 Hapis Lab. All rights reserved.
 //
 
-#include "lib.h"
+#include "lib.hpp"
 
 int main() { SayHello(); }
